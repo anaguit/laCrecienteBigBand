ab0aeafd3846fa00d0ca770246ce037010e3be92
Ana Santoandre <anasantoandre84@gmail.com>
2022-12-06 11:14:11 -0300

    rutas, metodos y vistas secundarias

--- src/controller/crecienteController.js
+++ src/controller/crecienteController.js
@@ -1,6 +1,15 @@
 const crecienteController = {
     inicio:(req,res)=>{
         res.render("inicio")
+    },
+    nosotros:(req,res)=>{
+        res.render("nosotros")
+    },
+    trayectoria:(req,res)=>{
+        res.render("trayectoria")
+    },
+    propuesta:(req,res)=>{
+        res.render("propuesta")
     }
 };
 
--- src/routers/crecienteRouter.js
+++ src/routers/crecienteRouter.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const crecienteController = require("../controller/crecienteController");
+
+
 router.get("/",crecienteController.inicio);
+router.get("/nosotros",crecienteController.nosotros);
+router.get("/trayectoria",crecienteController.trayectoria);
+router.get("/propuesta",crecienteController.propuesta);
 
 module.exports = router;
